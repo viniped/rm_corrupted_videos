0c73f96a7ee1046265165d91798b491a32c8866c
vinicius-pedro <125911962+viniped@users.noreply.github.com>
2023-08-15 14:03:19 -0300

    Update corrupted.py

--- corrupted.py
+++ corrupted.py
@@ -1,7 +1,30 @@
 import subprocess
 from pathlib import Path
 import os
-import shutil
+from colorama import Fore
+import pyfiglet
+import random
+
+class Banner:
+    def __init__(self, banner):
+        self.banner = banner
+        self.lg = Fore.LIGHTGREEN_EX
+        self.w = Fore.WHITE
+        self.cy = Fore.CYAN
+        self.ye = Fore.YELLOW
+        self.r = Fore.RED
+        self.n = Fore.RESET
+
+    def print_banner(self):
+        colors = [self.lg, self.r, self.w, self.cy, self.ye]
+        f = pyfiglet.Figlet(font='slant')
+        banner = f.renderText(self.banner)
+        print(f'{random.choice(colors)}{banner}{self.n}')
+        print(f'{self.r}  Version: v0.0.1 https://github.com/viniped \n{self.n}')
+
+def show_banner():
+    banner = Banner('R M Corrupted Videos')
+    banner.print_banner()
 
 # Function to check if a video file has duration attribute
 def has_duration(file_path):
@@ -17,6 +40,9 @@ def delete_videos_without_duration(folder_path):
             print(f"Deleting {path}")
             os.remove(str(path))
 
+# Display the banner
+show_banner()
+
 # Ask the user to input the folder path
 folder_path = input("Please enter the folder path: ")
 
@@ -25,5 +51,5 @@ if not os.path.isdir(folder_path):
     print("Invalid folder path.")
 else:
     # Call the function to delete videos without duration
-    delete_videos_without_duration(folder_path)
+    delete_videos_without_duration(folder_path)    
     print("Deletion completed.")
